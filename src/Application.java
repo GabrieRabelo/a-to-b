992664c4c75442e4b0a99ba7feb333039d0eab18
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-05-30 17:50:03 -0300

    adiciona posicao dos personagens

--- src/Application.java
+++ src/Application.java
@@ -9,28 +9,40 @@ public class Application {
         int lineLength = 0;
         int columnLength = 0;
 
-        //primeira leitura do arquivo
         In arq = new In(FILE_NAME);
 
         while((aux = arq.readLine()) != null) {
             if(lineLength == 0) lineLength = aux.length();
             columnLength++;
         }
-
         arq.close();
 
         char[][] maze = new char[lineLength][columnLength];
+        int[] hero = new int[2];
+        int[] villain = new int[2];
 
-        //segunda leitura do arquivo
         arq = new In(FILE_NAME);
-        int columnPos=0;
+        int y=0;
+
+        Graph graph = new Graph(lineLength * columnLength);
 
         while((aux = arq.readLine()) != null) {
             char[] lineArray = aux.toCharArray();
-            for(int j=0; j<lineLength;j++){
-                maze[j][columnPos] = lineArray[j];
+
+            for(int x=0; x<lineLength;x++){
+                maze[x][y] = lineArray[x];
+
+                if (maze[x][y] == 'A') {
+                    hero[0] = x;
+                    hero[1] = y;
+                }
+                if (maze[x][y] == 'B') {
+                    villain[0] = x;
+                    villain[1] = y;
+                }
             }
-            columnPos++;
+
+            y++;
         }
 
         for(int i = 0; i<columnLength;i++) {
@@ -42,7 +54,9 @@ public class Application {
 
         arq.close();
 
-        System.out.println("Tamanho da linha: " + lineLength + "\nTamanho da coluna: " + columnLength);
+//        System.out.println("Tamanho da linha: " + lineLength + "\nTamanho da coluna: " + columnLength);
+        System.out.println("Posição heroi linha: " + hero[0] + " coluna: " + hero[1]);
+        System.out.println("Posição vilao linha: " + villain[0] + " coluna: " + villain[1]);
 
     }
 }
