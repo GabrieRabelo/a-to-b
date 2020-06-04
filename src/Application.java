e5754f84a13e159c379a683e5195d6d11371cba3
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-06-04 11:55:49 -0300

    some code cleaning

--- src/Application.java
+++ src/Application.java
@@ -4,13 +4,14 @@ public class Application {
     public static void main(String[] args) {
 
         long startTime = System.currentTimeMillis();
-        final String FILE_NAME= "caso7_cohen.txt";
+        final String FILE_NAME= "caso1_cohen.txt";
 
         Maze maze = new Maze(FILE_NAME);
 
         long endTime = System.currentTimeMillis();
+
         System.out.println(endTime-startTime + "ms");
-        System.out.println("Largura: " + maze.getWidth() + "\nAltura: " + maze.getHeigth());
+        System.out.println("Largura: " + maze.getWidth() + "\nAltura: " + maze.getHeight());
         System.out.println("Posição heroi linha: " + maze.getHero()[0] + " coluna: " + maze.getHero()[1]);
         System.out.println("Posição vilao linha: " + maze.getVillain()[0] + " coluna: " + maze.getVillain()[1]);
 
--- src/Maze.java
+++ src/Maze.java
@@ -1,25 +1,20 @@
 public class Maze {
-    private int width, heigth;
+    private int width;
+    private int height;
     private final int[] hero = new int[2];
     private final int[] villain = new int[2];
     private Graph graph;
 
     public Maze(String filename){
-        In arq = new In(filename);
-        String aux = "";
+        getFileSize(filename);
 
-        while((aux = arq.readLine()) != null) {
-            if(width == 0) width = aux.length();
-            heigth++;
-        }
-        arq.close();
-
-        int[][] maze = new int[width][heigth];
-
-        arq = new In(filename);
-
+        int[][] maze = new int[width][height];
         int y=0;
         int count = 3;
+
+        In arq = new In(filename);
+        String aux;
+
         while((aux = arq.readLine()) != null) {
             char[] lineArray = aux.toCharArray();
             for(int x = 0; x< width; x++){
@@ -41,21 +36,35 @@ public class Maze {
                         villain[0] = x;
                         villain[1] = y;
                         break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + lineArray[x]);
                 }
             }
             y++;
         }
         arq.close();
 
-        //createGraph(maze);
+        createGraph(maze);
+
         //printMaze(maze);
     }
 
+    public void getFileSize(String filename) {
+        In arq = new In(filename);
+        String aux;
+
+        while((aux = arq.readLine()) != null) {
+            if(width == 0) width = aux.length();
+            height++;
+        }
+        arq.close();
+    }
+
     public void createGraph(int [][] maze) {
-        graph = new Graph(width*heigth);
+        graph = new Graph(width* height);
 
         for(int y = 1; y<width-1;y++){
-            for(int x = 1; x<heigth;x++){
+            for(int x = 1; x< height; x++){
                 if(maze[y][x] != 0) {
                     if(maze[y][x+1] != 0) {
                         graph.addEdge(maze[y][x], maze[y][x+1]);
@@ -69,7 +78,7 @@ public class Maze {
     }
 
     public void printMaze(int[][] maze) {
-        for(int i = 0; i< heigth; i++) {
+        for(int i = 0; i< height; i++) {
             for(int j = 0; j<width;j++) {
                 System.out.print(maze[i][j] + " ");
             }
@@ -90,11 +99,8 @@ public class Maze {
         return width;
     }
 
-    public int getHeigth(){
-        return heigth;
+    public int getHeight(){
+        return height;
     }
 
-    public Graph getGraph() {
-        return graph;
-    }
 }
